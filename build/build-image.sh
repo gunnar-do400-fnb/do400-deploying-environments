593885bc602199dff1c56a9239f0b454f7c75aaa
Gunnar Hilling <ghilling@redhat.com>
2024-09-13 06:11:16 -0400

    add build number

--- build/build-image.sh
+++ build/build-image.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 ./mvnw package -DskipTests \
                 -Dquarkus.container-image.push=true \
                 -Dquarkus.container-image.build=true \
+                -Dquarkus.container-image.tag=build-${BUILD_NUMBER} \
                 -Dquarkus.container-image.username=$QUAY_USR \
                 -Dquarkus.container-image.password=$QUAY_PSW
